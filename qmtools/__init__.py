9bd0afbd91e823e6fa552497168211c6c91df301
Mehmet Canturk <canturk.iqc@gmail.com>
2018-05-08 23:58:48 +0300

    modified:   qmtools/__init__.py 	modified:   qmtools/qpar.py 	modified:   qmtools/qsim.py 	modified:   qmtools/quad.py 	modified:   qmtools/qutil.py

---
+++ qmtools/__init__.py
@@ -0,0 +1,30 @@
+###############################################################################
+# ToolBox Quadrature Measurement Protocol for a Harmonic Oscillator (HO)      #
+# Coupled to a Flux Qubit                                                     #
+###############################################################################
+# Author : Mehmet Canturk 
+# Date   : March 13, 2017
+###############################################################################
+#                NUMERICAL SIMULATIONS FOR QUATRATURE MEASUREMENT
+###############################################################################
+
+__all__ =['qpar', 'qutil', 'quad', 'qsim'];
+
+from qmtools import qpar
+from qmtools import qutil
+from qmtools import quad
+from qmtools import qsim
+#from qmtools import btree
+
+#
+# quad.py: contains classes for quadrature measurement, fock, thermal, and coherent states
+#
+#
+# qsim.py: contains Quadrature simulation class
+#
+#
+# qpar.py: contains a dictionary for system parameters
+#
+#
+# qutil.py: contains the required functions for time-dependent Hamiltonians
+#
